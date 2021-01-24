420e2feb767eb53ba482cf509313c98a41f3b850
AnjaliDileep-coder <68942211+AnjaliDileep-coder@users.noreply.github.com>
2021-01-24 13:13:45 +0530

    Update SplashScreen.js

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -7,20 +7,20 @@ import { ActivityIndicator } from 'react-native';
 
 export default class SplashScreen extends Component {
 
-    componentDidMount(){
-setTimeout(() => {
-    this.props.navigation.navigate('MainScreen')
-}, 2000);
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.navigation.navigate('MainScreen')
+        }, 2000);
     }
 
-render() {
-    return (
-        <View style={{flex:1, justifyContent:'center', alignItems: 'center', backgroundColor:'#fff'}}>
-            <Image source={require('../assets/logo.jpg')}
-                style={{width:'100%',height:'30%'}}/>
-                <ActivityIndicator size="large" color= '#00BFFF'
-                style={{margin:10}}/>
-                </View>
-    );
-};
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+                <Image source={require('../assets/logo.jpg')}
+                    style={{ width: '100%', height: '30%' }} />
+                <ActivityIndicator size="large" color='#00BFFF'
+                    style={{ margin: 10 }} />
+            </View>
+        );
+    };
 }
